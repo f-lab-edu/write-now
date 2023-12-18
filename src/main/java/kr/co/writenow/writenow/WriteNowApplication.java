ffd8ab15501322789f7ba989369b691c151b2ce5
seungheon <seungheon@apexsoft.co.kr>
2023-12-19 00:49:13 +0900

    #WRI-8 프로젝트 생성

---
+++ src/main/java/kr/co/writenow/writenow/WriteNowApplication.java
@@ -0,0 +1,13 @@
+package kr.co.writenow.writenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteNowApplication.class, args);
+	}
+
+}
