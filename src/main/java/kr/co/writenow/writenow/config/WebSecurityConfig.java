f61cf1951c9ac234be5adc11425fbe12e350ebbb
seungheon <seungheon@apexsoft.co.kr>
2023-12-21 23:21:46 +0900

    hello world 찍기

---
+++ src/main/java/kr/co/writenow/writenow/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package kr.co.writenow.writenow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth->
+                auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
