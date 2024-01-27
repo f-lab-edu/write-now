ffda5b548f0169659a6c4b10f8bead5eeda3fa4a
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-01-27 14:44:48 +0900

    코드 포멧팅 및 불필요한 코드를 삭제합니다.

--- src/main/java/kr/co/writenow/writenow/repository/post/PostRepository.java
+++ src/main/java/kr/co/writenow/writenow/repository/post/PostRepository.java
@@ -4,4 +4,5 @@ import kr.co.writenow.writenow.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
--- src/main/java/kr/co/writenow/writenow/service/post/PostTagService.java
+++ src/main/java/kr/co/writenow/writenow/service/post/PostTagService.java
@@ -1,16 +1,18 @@
 package kr.co.writenow.writenow.service.post;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import kr.co.writenow.writenow.domain.post.Post;
 import kr.co.writenow.writenow.domain.post.PostTag;
 import kr.co.writenow.writenow.domain.tag.Tag;
-import kr.co.writenow.writenow.repository.post.PostTagRepository;
 import kr.co.writenow.writenow.repository.tag.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
-
-import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +20,6 @@ import java.util.*;
 public class PostTagService {
 
     private final TagRepository tagRepository;
-    private final PostTagRepository postTagRepository;
 
     public Set<PostTag> makePostTagSet(Post post, List<String> tagValues){
         if(ObjectUtils.isEmpty(tagValues)){
