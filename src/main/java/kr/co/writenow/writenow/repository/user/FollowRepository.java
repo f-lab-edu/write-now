cec837e52889df13d6e4f65d79e3f98cf6995741
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-01-29 00:53:47 +0900

    글 작성 시 피드를 발행하는 로직을 추가합니다.

--- src/main/java/kr/co/writenow/writenow/repository/user/FollowRepository.java
+++ src/main/java/kr/co/writenow/writenow/repository/user/FollowRepository.java
@@ -1,14 +1,15 @@
 package kr.co.writenow.writenow.repository.user;
 
+import java.util.List;
+import java.util.Optional;
 import kr.co.writenow.writenow.domain.user.Follow;
 import kr.co.writenow.writenow.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     Optional<Follow> findByFollowerAndFollowee(User followerUser, User followeeUser);
+    List<Follow> findByFollowee(User followee);
 
     void deleteByFollowerAndFollowee(User followerUser, User followeeUser);
 }
