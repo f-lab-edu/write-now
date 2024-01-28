2bfddeb7f3d135c0d00a8cede8e965a6ab7b1547
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-01-28 17:08:02 +0900

    빈 스트링 값 대신 Optional을 이용해 반환

--- src/main/java/kr/co/writenow/writenow/common/DateUtil.java
+++ src/main/java/kr/co/writenow/writenow/common/DateUtil.java
@@ -2,15 +2,16 @@ package kr.co.writenow.writenow.common;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 import org.springframework.util.ObjectUtils;
 
 public class DateUtil {
 
-  public static String datetimeConvertToString(LocalDateTime dateTime) {
+  public static Optional<String> datetimeConvertToString(LocalDateTime dateTime) {
     if (ObjectUtils.isEmpty(dateTime)) {
-      return "";
+      return Optional.empty();
     }
-    return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    return Optional.of(dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
   }
 
 }
