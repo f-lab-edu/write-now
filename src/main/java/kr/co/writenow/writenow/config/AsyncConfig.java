1eaad3507bf8a231eb9fe3d41ec11aee9befea1b
이승헌 <seungheon@apexsoft.co.kr>
2024-02-02 16:12:45 +0900

    스레드 풀 설정 값 yml 프로퍼티로 관리, 스레드에서 예외 발생 시 custom 클래스 사용

--- src/main/java/kr/co/writenow/writenow/config/AsyncConfig.java
+++ src/main/java/kr/co/writenow/writenow/config/AsyncConfig.java
@@ -1,22 +1,40 @@
 package kr.co.writenow.writenow.config;
 
-import org.springframework.context.annotation.Bean;
+import java.util.concurrent.Executor;
+import kr.co.writenow.writenow.exception.handler.CustomThreadExceptionHandler;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
-  @Bean
-  public TaskExecutor taskExecutor(){
+  @Value("${threadPool.core-size}")
+  private int coreSize;
+
+  @Value("${threadPool.max-size}")
+  private int maxSize;
+
+  @Value("${threadPool.queue-size}")
+  private int queueSize;
+
+  @Override
+  public Executor getAsyncExecutor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(5);
-    executor.setMaxPoolSize(100);
-    executor.setQueueCapacity(50);
+    executor.setCorePoolSize(coreSize);
+    executor.setMaxPoolSize(maxSize);
+    executor.setQueueCapacity(queueSize);
     executor.initialize();
     return executor;
   }
+
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return new CustomThreadExceptionHandler();
+  }
+
 }
